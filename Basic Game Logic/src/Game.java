590bf06e1a4af5f30b99d93df298fac19dcb12c9
up201505244 <up201505244@B03AV1210.salas.feupsig.fe.up.pt>
2017-02-21 17:48:04 +0000

    Fixed Game Class

--- Basic Game Logic/src/Game.java
+++ Basic Game Logic/src/Game.java
@@ -5,7 +5,6 @@ public class Game {
 	private static Hero hero;	
 	private static Guard guard;
 	public static void main(String[] args) {
-
 		Scanner p = new Scanner(System.in);
 		board = Board.GuardBoard();
 		hero = new Hero(1,1);
@@ -15,9 +14,36 @@ public class Game {
 				for (int j = 0; j < board.getLength(); j++) {
 					System.out.print(board.getSquare(i, j));
 
+				}
+				System.out.println();
+			}
+			char pos = p.next().charAt(0);
+			switch (pos) {
+			case 'w':
+				hero.move(0, -1, board, 1);
+				break;
+			case 's':
+				hero.move(0, 1, board, 1);
+				break;
+			case 'd':
+				hero.move(1, 0, board, 1);
+				break;
+			case 'a':
+				hero.move(-1, 0, board, 1);
+				break;
+			}
+			guard.move();
+		} while (board.getSquare(5, 0) != 'H' && board.getSquare(6, 0) != 'H' && board.getSquare(hero.GetPositionY(), hero.GetPositionX()+1) != 'G' && board.getSquare(hero.GetPositionY()-1, hero.GetPositionX()) != 'G'
+				&& board.getSquare(hero.GetPositionY(), hero.GetPositionX()-1) != 'G' && board.getSquare(hero.GetPositionY()+1, hero.GetPositionX()) != 'G' && (hero.GetPositionX()!=guard.GetPositionX()&&hero.GetPositionY()!=guard.GetPositionY()));
+		if (board.getSquare(5, 0)== 'H' || board.getSquare(6, 0) == 'H')
+//			OgreLevel(p);
+			System.out.println("Game V");
+
+		else
+			System.out.println("Game Over");
+		p.close();
 	}
 
-				
 //	public static void OgreLevel(Scanner p) {
 //		int y = 8, x = 1, ox = 4, oy = 1;
 //		char board[][] = OgreBoard();
@@ -159,165 +185,4 @@ public class Game {
 //		else
 //			System.out.println("Game Over");
 //	}
-=======
-	
-	public static void OgreLevel()
-	{
-		Scanner p = new Scanner(System.in);
-		int y=8, x=1,ox=4,oy=1;
-		char board[][]=new char[10][10];
-		for(int i=0;i<board.length;i++)
-		{
-			for(int j=0;j<board.length;j++) 
-				board[i][j] = ' ';
-			if (i==0||i==9)
-			{
-				for(int j=0;j<board[i].length;j++)
-				{
-				board[i][j]='X';
-				}
-			}
-			else 
-			{
-				board[i][1]='X';
-				board[i][9]='X';
-			}
-		}
-		board[1][0]='I';
-		board[1][4]='O';
-		board[1][8]='k';
-		board[8][1]='H';
-		do
-		{
-			for(int i=0;i<board.length;i++)
-			{
-				for(int j=0;j<board[i].length;j++)
-				{
-					System.out.print(board[i][j]);
-					
-				}
-				System.out.println();
-			}
-			char pos = p.next().charAt(0);
-			switch(pos)
-			{
-			case 'w':
-				if(board[y-1][x]==' ')
-				{
-					board[y][x]=' ';
-					y=y-1;
-					board[y][x]='H';
-				}
-				else if(board[y][x-1]=='k')
-				{
-					board[y][x]=' ';
-					x=x-1;
-					board[y][x]='K';
-				}
-				break;
-			case 's':
-				if(board[y+1][x]==' ')
-				{
-					board[y][x]=' ';
-					y=y+1;
-					board[y][x]='H';
-				}
-				break;
-			case 'd':
-				if(board[y][x+1] == ' ')
-				{
-					board[y][x]=' ';
-					x = x + 1;
-					board[y][x]='H';
-				}
-				else if(board[y][x-1]=='k')
-				{
-					board[y][x]=' ';
-					x=x-1;
-					board[y][x]='K';
-				}
-				break;
-			case 'a':
-				if(board[y][x-1]==' ')
-				{
-					board[y][x]=' ';
-					x=x-1;
-					board[y][x]='H';
-				}
-				else if(board[y][x-1]=='I'&&board[y][x]=='K')
-				{
-					board[y][x-1]='S';
-				}
-				else if(board[y][x-1]=='S')
-				{
-					board[y][x]=' ';
-					x=x-1;
-					board[y][x]='H';
-				}
-			do
-			{	
-				Random ran = new Random();
-				int Ogrepos = ran.nextInt(4);
-				int tmpox=ox,tmpoy=oy//posição do ogre
-				switch(Ogrepos)
-				case 0://w
-					if(board[oy-1][ox]==' ')
-					{
-						board[oy][ox]=' ';
-						oy=oy-1;
-						board[oy][ox]='O';
-					}
-					else if(board[oy][ox-1]=='k')
-					{
-						board[oy][ox]=' ';
-						ox=ox-1;
-						board[oy][ox]='$';
-					}
-					break;
-				case 1://s
-					if(board[oy+1][ox]==' ')
-					{
-						board[oy][ox]=' ';
-						oy=oy+1;
-						board[oy][ox]='O';
-					}if(oy=2&&ox=8)
-					{
-						board[oy-1][ox]='k';
-					}
-					break;
-				case 2://d
-					if(board[oy][ox+1] == ' ')
-					{
-						board[oy][ox]=' ';
-						ox = ox + 1;
-						board[oy][ox]='O';
-					}
-					else if(board[oy][ox-1]=='k')
-					{
-						board[oy][ox]= ' ';
-						ox = ox-1;
-						board[oy][ox] = '$';
-					}
-					break;
-				case 3://a
-					if(board[oy][ox-1]==' ')
-					{
-						board[oy][ox]=' ';
-						ox=ox-1;
-						board[oy][ox]='O';
-					}
-					if(oy=1&&ox=7)
-					{
-						board[oy][ox+1]='k';
-					}
-					break;
-			}
-			while(oy==tmpoy&&ox==tmpox)//verifica se o ogre fica no mesmo sitio
-		}while(board[1][0]!='H'&&board[y][x+1]!='O'&&board[y-1][x]!='O'&&board[y][x-1]!='O'&&board[y+1][x]!='O' && board[y][x]!=board[oy][ox]));
-		if(board[1][0]=='H')
-			System.out.print("Victory");
-		if(board[y][x+1]=='O'||board[y-1][x]=='O'||board[y][x-1]=='O'||board[y+1][x]=='O' || board[y][x]==board[oy][ox])
-			System.out.print("Game Over");
-	}
->>>>>>> branch 'master' of https://github.com/RicardoBoia2017/LPOO1617_T2G10
 }
